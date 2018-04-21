7758b92da874f7a3c5ab6f863d12232256dda5ac
hobbyquaker <bastiraff@googlemail.com>
2018-04-21 12:37:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# node-red-contrib-combine
+
+[![NPM version](https://badge.fury.io/js/node-red-contrib-combine.svg)](http://badge.fury.io/js/node-red-contrib-combine)
+[![Dependency Status](https://img.shields.io/gemnasium/hobbyquaker/node-red-contrib-combine.svg?maxAge=2592000)](https://gemnasium.com/github.com/hobbyquaker/node-red-contrib-combine)
+[![Build Status](https://travis-ci.org/hobbyquaker/node-red-contrib-combine.svg?branch=master)](https://travis-ci.org/hobbyquaker/node-red-contrib-combine)
+[![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/sindresorhus/xo)
+[![License][mit-badge]][mit-url]
+
+> Node-RED Nodes that outputs combinations of consecutive incoming messages
+
+
+## Nodes
+
+#### Statistic
+
+Calculate statistic samples like e.g. min, max, mean, median, ... from incoming payloads.
+ 
+
+## License
+
+MIT (c) Sebastian Raff
+
+[mit-badge]: https://img.shields.io/badge/License-MIT-blue.svg?style=flat
+[mit-url]: LICENSE
---
+++ nodes/combine-statistic.js
@@ -0,0 +1,145 @@
+module.exports = function (RED) {
+    class CombineStatisticNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+
+            this.msgs = {};
+            this.timeouts = {};
+
+            this.topic = config.topic || '';
+            this.operator = config.operator;
+            this.distinction = config.distinction || 'topic';
+            this.defer = parseInt(config.defer, 10) || 0;
+            this.timeout = parseInt(config.timeout, 10) || 0;
+
+            this.on('input', this.handleMsg);
+        }
+
+        handleMsg(incoming) {
+            const key = incoming[this.distinction];
+            this.msgs[key] = incoming.payload;
+            const outgoing = this.combine();
+
+            if (this.defer) {
+                this.sendDeferred(outgoing);
+            } else {
+                this.send(outgoing);
+            }
+
+            if (this.timeout) {
+                clearTimeout(this.timeouts[key]);
+                this.timeouts[key] = setTimeout(() => {
+                    this.remove(key);
+                }, this.timeout);
+            }
+        }
+
+        remove(key) {
+            delete this.msgs[key];
+            const msg = this.combine();
+
+            this.send(msg);
+        }
+
+        sendDeferred(msg) {
+            this.msgDeferred = msg;
+            if (!this.sendTimeout) {
+                this.sendTimeout = setTimeout(() => {
+                    this.send(this.msgDeferred);
+                    this.sendTimeout = null;
+                }, this.defer);
+            }
+        }
+
+        combine() {
+            const topics = Object.keys(this.msgs);
+            const payloads = Object.values(this.msgs);
+
+            const len = payloads.length;
+
+            for (let i = 0; i < len; i++) {
+                payloads[i] = Number(payloads[i]) || 0;
+            }
+
+            const min = Math.min.apply(null, payloads);
+            const max = Math.max.apply(null, payloads);
+            const range = max - min;
+            const midrange = min + ((max - min) / 2);
+            const sum = this.sum(payloads);
+            const mean = sum / len;
+            const median = this.median(payloads);
+            const modes = this.modes(payloads);
+            const variance = this.mean(payloads.map(num => Math.pow(num - mean, 2)));
+            const standardDeviation = Math.sqrt(variance);
+            const meanAbsoluteDeviation = this.mean(payloads.map(num => Math.abs(num - mean)));
+            const zScores = payloads.map(num => (num - mean) / standardDeviation);
+
+            const combine = {
+                topics: this.distinction === 'topic' ? topics : undefined,
+                messages: this.distinction === '_msgid' ? topics : undefined,
+                len,
+                sum,
+                min,
+                max,
+                range,
+                midrange,
+                mean,
+                median,
+                modes,
+                variance,
+                standardDeviation,
+                meanAbsoluteDeviation,
+                zScores
+            };
+
+            return Object.assign({
+                topic: this.topic,
+                payload: combine[this.operator]
+            }, combine);
+        }
+
+        sum(array) {
+            return array.reduce((pv, cv) => pv + cv, 0);
+        }
+
+        mean(array) {
+            return this.sum(array) / array.length;
+        }
+
+        median(array) {
+            array.sort((a, b) => a - b);
+            const mid = array.length / 2;
+            if (mid % 1) {
+                return array[mid - 0.5];
+            }
+            return (array[mid - 1] + array[mid]) / 2;
+        }
+
+        modes(array) {
+            if (array.length === 0) {
+                return array;
+            }
+            const modeMap = {};
+            let maxCount = 1;
+            let modes = [array[0]];
+
+            array.forEach(val => {
+                if (modeMap[val]) {
+                    modeMap[val] += 1;
+                } else {
+                    modeMap[val] = 1;
+                }
+                if (modeMap[val] > maxCount) {
+                    modes = [val];
+                    maxCount = modeMap[val];
+                } else if (modeMap[val] === maxCount) {
+                    modes.push(val);
+                    maxCount = modeMap[val];
+                }
+            });
+            return modes;
+        }
+    }
+
+    RED.nodes.registerType('combine-statistic', CombineStatisticNode);
+};
