0d801810a43a62f3497107bb50526963ebc8e0c6
hobbyquaker <bastiraff@googlemail.com>
2018-06-16 11:43:52 +0200

    add if node

---
+++ nodes/combine-if.js
@@ -0,0 +1,21 @@
+module.exports = function (RED) {
+    class CombineIfNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            this.status({fill: 'grey', shape: 'ring', text: 'false'});
+            this.on('input', msg => {
+                if (msg.topic === config.topic) {
+                    this.state = Boolean(msg.payload);
+                    if (this.state) {
+                        this.status({fill: 'blue', shape: 'dot', text: 'true'});
+                    } else {
+                        this.status({fill: 'grey', shape: 'ring', text: 'false'});
+                    }
+                } else if (this.state) {
+                    this.send(msg);
+                }
+            });
+        }
+    }
+    RED.nodes.registerType('combine-if', CombineIfNode);
+};
