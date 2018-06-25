3eaabafc5164bbb8fef9e17c6d16d2f9876f293b
hobbyquaker <bastiraff@googlemail.com>
2018-06-25 18:08:45 +0200

    rename node timeout to defer

---
+++ nodes/combine-defer.js
@@ -0,0 +1,57 @@
+module.exports = function (RED) {
+    class CombineDeferNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+
+            this.state = {};
+            this.timer = {};
+            this.on('input', msg => {
+                if (msg.payload) {
+                    /*
+                    let match;
+                    switch (config.condition) {
+                        case 'eq':
+                            match = msg.payload === config.tPayload;
+                            break;
+                        case 'neq':
+                            match = msg.payload !== config.tPayload;
+                            break;
+                        case 'gt':
+                            match = msg.payload > config.payload;
+                            break;
+                        case 'lt':
+                            match = msg.payload < config.payload;
+                            break;
+                        case 'gte':
+                            match = msg.payload <= config.payload;
+                            break;
+                        case 'lte':
+                            match = msg.payload >= config.payload;
+                            break;
+                        case 'btwn':
+                            match = (msg.payload >= config.lower) && (msg.payload <= config.upper);
+                            break;
+                        default:
+                    }
+                    */
+
+                    if (!this.timer[msg.topic] && !this.state[msg.topic]) {
+                        this.state[msg.topic] = true;
+                        this.timer[msg.topic] = setTimeout(() => {
+                            this.timer[msg.topic] = null;
+                            this.send(msg);
+                            this.status({fill: 'blue', shape: 'dot'});
+                        }, config.timeout * 1000);
+                        this.status({fill: 'blue', shape: 'ring'});
+                    }
+                } else {
+                    clearTimeout(this.timer[msg.topic]);
+                    this.timer[msg.topic] = null;
+                    this.state[msg.topic] = false;
+                    this.status({});
+                }
+            });
+        }
+    }
+    RED.nodes.registerType('combine-defer', CombineDeferNode);
+};
