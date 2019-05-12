641145008bc90803ea20a76c0fd1da3e3b10f25d
hobbyquaker <bastiraff@googlemail.com>
2019-05-12 22:07:55 +0200

    defer node improvements: invert, timeout via msg/context/env (#12)

--- nodes/combine-defer.js
+++ nodes/combine-defer.js
@@ -3,52 +3,67 @@ module.exports = function (RED) {
         constructor(config) {
             RED.nodes.createNode(this, config);
 
+            if (config.invert === 'false') {
+                config.invert = false;
+            } else {
+                config.invert = Boolean(config.invert);
+            }
+
+            this.config = config;
+
             this.state = {};
             this.timer = {};
+
             this.on('input', msg => {
-                /*
-                let match;
-                switch (config.condition) {
-                    case 'eq':
-                        match = msg.payload === config.tPayload;
-                        break;
-                    case 'neq':
-                        match = msg.payload !== config.tPayload;
-                        break;
-                    case 'gt':
-                        match = msg.payload > config.payload;
-                        break;
-                    case 'lt':
-                        match = msg.payload < config.payload;
-                        break;
-                    case 'gte':
-                        match = msg.payload <= config.payload;
-                        break;
-                    case 'lte':
-                        match = msg.payload >= config.payload;
-                        break;
-                    case 'btwn':
-                        match = (msg.payload >= config.lower) && (msg.payload <= config.upper);
-                        break;
-                    default:
-                }
-                */
-                if (msg.payload) {
-                    /* istanbul ignore else */
-                    if (!this.timer[msg.topic] && !this.state[msg.topic]) {
-                        this.state[msg.topic] = true;
-                        this.timer[msg.topic] = setTimeout(() => {
-                            this.timer[msg.topic] = null;
-                            this.send(msg);
-                            this.status({fill: 'blue', shape: 'dot'});
-                        }, config.timeout * 1000);
-                        this.status({fill: 'blue', shape: 'ring'});
-                    }
-                } else {
+                if (Boolean(msg.payload) === config.invert) {
                     clearTimeout(this.timer[msg.topic]);
                     this.timer[msg.topic] = null;
                     this.state[msg.topic] = false;
                     this.status({});
+                } else if (!this.timer[msg.topic] && !this.state[msg.topic]) {
+                    this.state[msg.topic] = true;
+
+                    this.getTimeout(msg).then(timeout => {
+                        this.timer[msg.topic] = setTimeout(() => {
+                            this.timer[msg.topic] = null;
+                            this.send(msg);
+                            this.status({fill: 'blue', shape: 'dot'});
+                        }, timeout * 1000);
+                        this.status({fill: 'blue', shape: 'ring'});
+                    });
+                }
+            });
+        }
+
+        getTimeout(msg) {
+            return new Promise((resolve, reject) => {
+                const type = this.config.timeoutType;
+                const val = this.config.timeout;
+
+                switch (type) {
+                    case 'msg':
+                        resolve(RED.util.getMessageProperty(msg, val));
+                        break;
+
+                    case 'flow':
+                    case 'global': {
+                        const contextKey = RED.util.parseContextStore(val);
+                        this.context()[type].get(contextKey.key, contextKey.store, (err, res) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve(res);
+                            }
+                        });
+                        break;
+                    }
+
+                    case 'env':
+                        resolve(RED.util.evaluateNodeProperty(val, 'env', this));
+                        break;
+
+                    default:
+                        resolve(val);
                 }
             });
         }
